f50734b74ba7c9aa3ad70e25effdf4c154d2ed71
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-23 01:57:05 +0200

    Container component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createGlobalStyle } from "styled-components";
+import Container from 'components/Container/Container'
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -14,7 +15,7 @@ function App() {
     <BrowserRouter>
       <GlobalStyles />
       <>
-        // Content here
+        <Container />
       </>
     </BrowserRouter>
   );
---
+++ src/components/Container/Container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from 'components/layout/Navbar';
+
+export default function Container() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
