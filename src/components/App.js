ab946081c05667e7be9ee23ce9c27ab711f8fec3
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-22 22:08:58 +0200

    createGlobalStyles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Work+Sans&display=swap');
+    font-family: 'Work Sans', sans-serif;
+  }
+`
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <>
+        // Content here
       </>
     </BrowserRouter>
   );
