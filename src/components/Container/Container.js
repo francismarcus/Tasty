1d78008a0dd6e0f27f13396c42a1067dc607664b
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-23 04:51:55 +0200

    add <Feed /> to Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from 'components/layout/Navbar';
+import Feed from 'components/feed/Feed';
 
 export default function Container() {
   return (
     <>
       <Navbar />
+      <Feed />
     </>
   );
 }
