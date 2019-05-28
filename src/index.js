990965449458181a7fd14d4577de92bd53ea6734
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-28 08:34:46 +0200

    compose store enhancers & connect redux to firestore

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from 'reducers';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import config from 'Fire';
 
+const store = createStore(reducers,
+  compose(
+   applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+   reduxFirestore(config),
+   reactReduxFirebase(config)
+  )
+);
 ReactDOM.render(
-    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+    <Provider store={store}>
       <App />
     </Provider>,
     document.getElementById('root')
