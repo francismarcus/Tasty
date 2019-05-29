f01864fdd6809e9db82edbc409bf642d23e0339a
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-29 03:28:43 +0200

    no nested object without the dummy data

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,16 +1,10 @@
-
 const recipeReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'NEW_RECIPE':
-        return {
-          ...state, 
-            recipes: 
-              [...state.recipes, // => copy the nested object 
-                 action.payload] 
-            }
-      default: 
-        return state;
-    }
+  switch (action.type) {
+    case 'NEW_RECIPE':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
 
 export default recipeReducer;
